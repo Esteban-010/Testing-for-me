491371a83da7ddf062229b5e834662e7b65531ec
Esteban <esteban.blocked@gmail.com>
2020-11-07 13:35:16 -0500

    agregando el name a los heroes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const batman = {
+    name = "Batman",
     peso : 80,
     talla : 180,
     ataque : 150 
@@ -8,7 +9,8 @@ console.log(batman)
 
 
 class Heroe {
-    constructor(peso, talla,ataque){
+    constructor(name,peso, talla,ataque){
+        this.name = name
         this.peso = peso
         this.talla = talla
         this.ataque = ataque
