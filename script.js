89884f76a39410c7e137dae0adb2a21ba32a7e47
Esteban <esteban.blocked@gmail.com>
2020-11-07 14:09:20 -0500

    cambios en el script

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const batman = {
-<<<<<<< HEAD
-    name = "Batman",
-=======
     nombre : "Batman",
->>>>>>> rama-script
     peso : 80,
     talla : 180,
     ataque : 150 
@@ -13,13 +9,8 @@ console.log(batman)
 
 
 class Heroe {
-<<<<<<< HEAD
-    constructor(name,peso, talla,ataque){
-        this.name = name
-=======
     constructor(nombre,peso, talla,ataque){
         this.nombre = nombre
->>>>>>> rama-script
         this.peso = peso
         this.talla = talla
         this.ataque = ataque
